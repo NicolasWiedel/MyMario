f42c0559b11f3d7b82b53289177a391e0d0e0456
NICOLASWIEDEL\Nicolas Wiedel <nicolaswiedel@googlemail.com>
2019-03-06 21:47:27 +0100

    Bewegung für Mario 2.

--- core/src/com/mariobros/sprites/Mario.java
+++ core/src/com/mariobros/sprites/Mario.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.mariobros.GameConfig;
 
 /**
  * Created by Nicolas Wiedel on 06.03.2019.
@@ -22,14 +23,18 @@ public class Mario extends Sprite {
 
     public void defineMario(){
         BodyDef bdef = new BodyDef();
-        bdef.position.set(32, 32);
+        bdef.position.set(32 / GameConfig.PPM, 32 / GameConfig.PPM);
         bdef.type = BodyDef.BodyType.DynamicBody;
         b2body = world.createBody(bdef);
 
         FixtureDef fdef = new FixtureDef();
         CircleShape shape = new CircleShape();
-        shape.setRadius(5);
+        shape.setRadius(5 / GameConfig.PPM);
         fdef.shape = shape;
         b2body.createFixture(fdef);
+    }
+
+    public Body getB2body() {
+        return b2body;
     }
 }
